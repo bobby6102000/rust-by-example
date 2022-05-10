c6541054cea980d71e0b9b1fc5dd32f5a15a4935
bobby6102000 <52037988+bobby6102000@users.noreply.github.com>
2022-05-10 04:10:23 -0400

    Made first programe
    Comments and print hello world in rust

---
+++ helloworld/hello.rs
@@ -0,0 +1,7 @@
+/*Rust is modern programming language which is used by many modern companies and is somewath written like C or C++ 
+and is usually compared to python and has both of them have many similarities */
+
+fn main()
+{
+    println!("Hello World and welcome to learning rust with mew");
+}
